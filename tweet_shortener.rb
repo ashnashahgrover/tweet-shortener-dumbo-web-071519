d64e42feb0a2f254c8d676fd6d50867ec76f6cfa
Ashna Shah-Grover <as19@williams.edu>
2019-05-17 23:00:43 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,10 @@ end
 def shortened_tweet_truncator(tweet)
   new_tweet=word_substituter(tweet)
   if new_tweet.length > 140
-  new_tweet = new_tweet[0..139] + "..."
+    new_tweet = new_tweet[0..139] + "..."
+  end 
+  new_tweet
+end 
   
   
         
