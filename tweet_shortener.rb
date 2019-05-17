af82d0e9ab95c2cd08b1248a91e2062a47ec8bd5
Ashna Shah-Grover <as19@williams.edu>
2019-05-17 22:39:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,5 +2,11 @@
 
 def dictionary
   {
-    
+    "hello" becomes 'hi'
+    "to, two, too" become '2' 
+    "for, four" become '4'
+    'be' becomes 'b'
+    'you' becomes 'u'
+    "at" becomes "@" 
+    "and" becomes "&"
   }
