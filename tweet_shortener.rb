ee7348f17499cf244b3897e3e0771e24390c79ab
Ashna Shah-Grover <as19@williams.edu>
2019-05-17 22:40:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,12 +2,12 @@
 
 def dictionary
   {
-    "hello" => 'hi'
-    "to, two, too" => '2' 
-    "for, four" => '4'
-    'be' => 'b'
-    'you' => 'u'
-    "at" => "@" 
+    "hello" => 'hi',
+    "to, two, too" => '2', 
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
     "and" => "&"
   }
 end 
